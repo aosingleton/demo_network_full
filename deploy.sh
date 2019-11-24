925576c774a3fd12f33019fc4a6c690067486fde
Arthur Singleton <DAEDirectory\asingleton@a-34r79tnp0nee3.directory.digitalageexperts.com>
2019-11-23 21:15:25 -0500

    updated parameter and deploy files with btter stack naming convention.

--- deploy.sh
+++ deploy.sh
@@ -1,36 +1,39 @@
-#!/bin/bash -x
+#!/bin/bash
 
 # Description: Script creates sample demo network environment.  AWS cloud resources are included in the network cloudfomration/network file.
 
 action=$1
-varStage=$2
-varKeyPair=$3
-currentDate=$(date +"%m%d20%y.%H%M")
-
-# resource variables (s3 bucket name, local CF dir, stack name)
-varTempBucket=astempbucket$currentDate
-varCFBucketLocation=$PWD/correctYaml
-varTempCFStackName=${varStage}TempCFStackName071719
-
-# creating cloudformation bucket
-aws s3api create-bucket --bucket $varTempBucket
-syncing s3 bucket with local CF directory correctYaml
-aws s3 sync $varCFBucketLocation s3://$varTempBucket
+varKeyPair=$2
+varTempBucket=$3
+varProjectName=$4
+varProjectStage=$5
+currentDataAgain=$(date +"%m%d20%y-%H%M")
+varTempCFStackName=${varProjectName}-${varProjectStage}-stack
+aws s3 sync cloudformation/ s3://$varTempBucket/cloudformation
 
 echo "...creating parameters file"
 echo "
 [
     {
-        \"ParameterKey\": \"TemplatesNetwork\",
+        \"ParameterKey\": \"TempEC2KeyPair\",
         \"ParameterValue\": \"$varKeyPair\"
     },
     {
         \"ParameterKey\": \"TemplatesNetwork\",
-        \"ParameterValue\": \"https://${varTempBucket}.s3.amazonaws.com/network.yaml\"
+        \"ParameterValue\": \"https://${varTempBucket}.s3.amazonaws.com/cloudformation/network.yaml\"
     }
 ]
 " > parameters.json
 
+echo "...saving stack name in a file"
+echo "
+[
+    {
+        \"ParameterKey\": \"StackName\",
+        \"ParameterValue\": \"$varTempCFStackName\"
+    }
+]
+" > stack_name.json
 
 if [ $action == 'create' ]; then
     echo -e "Creating Demo Dashboard Project Resources. \n"
@@ -39,7 +42,11 @@ if [ $action == 'create' ]; then
 fi
 
 if [ $action == 'remove' ]; then
+    echo 'removing cloudformation bucket files'
+    aws s3 rm s3://$varTempBucket/cloudformation --recursive
+
     echo -e "Creating Demo Dashboard Project Resources. \n"
+    varName=$(cat stack_name.json | jq '.[0] | .ParameterValue' -r)
     aws cloudformation delete-stack --stack-name ${varTempCFStackName}
     aws cloudformation wait stack-delete-complete --stack-name $varTempCFStackName;
 fi
